5ed9f64ca2e01a7acf2f2bc0cc6f1b97490c82dd
Caio Carvalho <caio.cmalves@gmail.com>
2022-10-07 15:38:00 -0300

    Atualização das notas sobre a live 181

---
+++ Live181_strings/ex_mini_lang.py
@@ -0,0 +1,9 @@
+print(f'{"Eduardo":20}')
+
+print()
+nomes = ['Eduardo', 'João', 'Nietzsche']
+idades = [15, 26, 200]
+for nome, idade in zip(nomes, idades):
+    print(f'{nome:12} {idade:5}')
+    
+    
