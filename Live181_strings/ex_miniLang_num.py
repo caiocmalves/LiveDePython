5f1a957e1729b85d5284cb20be845c31ef09127f
Caio Carvalho <caio.cmalves@gmail.com>
2022-10-10 13:23:35 -0300

    finalização da live 181 de string

--- Live181_strings/ex_miniLang_num.py
+++ Live181_strings/ex_miniLang_num.py
@@ -5,3 +5,20 @@ print(f'{16:d}') #decimal
 print(f'{16:x}') #hexadecimal
 print(f'{16:X}') #hexadecimal maiúsculo
 
+
+from math import pi
+
+print(f'{pi:.2f}')
+print(f'{pi:.2e}')
+print(f'{pi:.2%}')
+print()
+
+#juntando
+print('{:-^10.4f}'.format(pi))
+
+
+#datas e horas
+from datetime import datetime
+
+print(f'{datetime.now():%d-%m-%y %H:%M:%S}')
+print(f'{datetime.now():%c}')
