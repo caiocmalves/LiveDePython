bf92cdebefa169258bdfd688383c8ae8dfad34a1
Caio Carvalho <caio.cmalves@gmail.com>
2022-10-10 10:54:55 -0300

    continuação da aula de formatação de strings

---
+++ Live181_strings/ex_miniLang_num.py
@@ -0,0 +1,7 @@
+#Conversão de bases
+print(f'{16:b}') #binário
+print(f'{16:o}') #octal
+print(f'{16:d}') #decimal
+print(f'{16:x}') #hexadecimal
+print(f'{16:X}') #hexadecimal maiúsculo
+
--- Live181_strings/ex_mini_lang.py
+++ Live181_strings/ex_mini_lang.py
@@ -5,5 +5,8 @@ nomes = ['Eduardo', 'João', 'Nietzsche']
 idades = [15, 26, 200]
 for nome, idade in zip(nomes, idades):
     print(f'{nome:12} {idade:5}')
-    
-    
+
+#Alinhamentos
+for nome, idade in zip(nomes, idades):
+    print(f'{nome:>12} {idade:<5}')
+
